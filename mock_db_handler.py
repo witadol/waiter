09af90f6b58760e65a0631f2148d629d3f4f982c
victor <pobedrik@gmail.com>
2017-08-18 18:49:41 +0300

    rab

---
+++ mock_db_handler.py
@@ -0,0 +1,9 @@
+MOCK_USERS = {'test@example.com': '123456'}
+
+
+class MockDBHelper:
+
+    def get_user(self, email):
+        if email in MOCK_USERS:
+            return MOCK_USERS[email]
+        return None
--- waiter.py
+++ waiter.py
@@ -1,12 +1,49 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from flask_login import LoginManager, login_required, logout_user
+from flask.ext.login import login_user
+from mock_db_handler import MockDBHelper as DBHelper
+from user import User
 
+
+DB = DBHelper()
 app = Flask(__name__)
+app.secret_key = '34e3MkXJjBkuBRYjRSzuJOSBMeWRdGWcePCWa3H8/cLnsBsh6Q4eGQWC0A7/SfbkGlTgaf/MGo0Q5HWoGqGciEhY3OTPAsoJPiY'
+login_manager = LoginManager(app)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route("/")
+def home():
+    return render_template("home.html")
 
+
+@app.route("/account")
+@login_required
+def account():
+    return "You are logged in"
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    email = request.form.get("email")
+    password = request.form.get("password")
+    user_password = DB.get_user(email)
+    if user_password and user_password == password:
+        user = User(email)
+        login_user(user, remember=True)
+        return redirect(url_for('account'))
+    return home()
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
+
+@login_manager.user_loader
+def load_user(user_id):
+    user_password = DB.get_user(user_id)
+    if user_password:
+        return User(user_id)
 
 if __name__ == '__main__':
     app.run()
